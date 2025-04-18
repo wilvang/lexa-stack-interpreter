7190282f8138b51b3ea5b6f99507b0a3b1c72422
Johan Fredrik Wilvang <johan@wilvang.com>
2025-04-18 20:28:14 +0200

    feat(parser): Implemented a parser to check if the token is an operation. This is achieved using a Data.Map structure.

--- src/Interpreter/Parser.hs
+++ src/Interpreter/Parser.hs
@@ -22,3 +22,27 @@ parseToken s =
     <|> maybeParseFloat s
     <|> maybeParseBool s
     <|> maybeParseContext s
+
+-- | Try to parse an operator by looking it up in a predefined table of operators.
+maybeParseOp :: String -> Maybe Token
+maybeParseOp s = TokOp <$> M.lookup s opTable
+  where
+    opTable = M.fromList
+      [ ("+", OpAdd)
+      , ("-", OpSub)
+      , ("*", OpMul)
+      , ("/", OpDiv)
+      , ("div", OpIDiv)
+      , ("<", OpLT)
+      , (">", OpGT)
+      , ("==", OpEQ)
+      , ("&&", OpAnd)
+      , ("||", OpOr)
+      , ("not", OpNot)
+      , ("exec", OpExec)
+      , ("if", OpIf)
+      , ("times", OpTimes)
+      , (":=", OpAssign)
+      , ("fun", OpFun)
+      ]
+
