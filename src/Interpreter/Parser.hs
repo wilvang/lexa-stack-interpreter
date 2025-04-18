6fd5d8fc76b8c9d8aab35e62a676a07c89467739
Johan Fredrik Wilvang <johan@wilvang.com>
2025-04-18 20:46:28 +0200

    feat(Parser): Created parsers for the contexts VString, Vlist and VQuotation. The data inside the context is validated trough using the parseTokens function for the inner context. This handles nested structures.

--- src/Interpreter/Parser.hs
+++ src/Interpreter/Parser.hs
@@ -2,6 +2,7 @@ module Interpreter.Parser where
 
 import Text.Read (readMaybe)
 import Data.Maybe (fromMaybe)
+import Data.List (uncons)
 import Control.Applicative ((<|>))
 import qualified Data.Map as M
 import Interpreter.Types
@@ -65,3 +66,43 @@ maybeParseBool :: String -> Maybe Token
 maybeParseBool "True"  = Just (TokVal (VBool True))
 maybeParseBool "False" = Just (TokVal (VBool False))
 maybeParseBool _       = Nothing
+
+-- | Try to parse a context-wrapped value such as a string, list, or quotation.
+-- It checks if the input string is wrapped in valid delimiters (e.g., double quotes for a string,
+-- square brackets for a list, or curly braces for a quotation).
+-- If the string is wrapped appropriately, it returns a corresponding 'Token'.
+-- Otherwise, it returns 'Nothing'.
+maybeParseContext :: String -> Maybe Token
+maybeParseContext s
+  | isWrapped ('"', '"') s = (Just . TokVal . VString . unwrap) s
+  | isWrapped ('[', ']') s = parseListContext s
+  | isWrapped ('{', '}') s = parseQuotationContext s
+  | otherwise              = Nothing
+
+-- | Check if a string is wrapped by the given opening and closing characters.
+-- The function checks if the first and last characters of the string match
+-- the specified 'open' and 'close' characters. Returns 'True' if they do, 'False' otherwise.
+isWrapped :: (Char, Char) -> String -> Bool
+isWrapped (open, close) s = case uncons s of
+  Just (first, rest) -> not (null rest) && last s == close && first == open
+  Nothing            -> False
+
+-- | Remove the first and last character from a string.
+-- Useful for unwrapping context-delimited strings like quotes or lists.
+unwrap :: String -> String
+unwrap = init . drop 1
+
+-- | Parse a string wrapped in curly braces as a quotation context.
+-- It treats the content inside the braces as a series of tokens, creating a 'TokVal' of 'VQuotation'.
+parseQuotationContext :: String -> Maybe Token
+parseQuotationContext =
+  fmap (TokVal . VQuotation) . traverse (Just . parseToken) . words . unwrap
+
+-- | Parse a string wrapped in square brackets as a list context.
+-- It treats the content inside the brackets as a series of tokens and converts them to 'VList' values.
+parseListContext :: String -> Maybe Token
+parseListContext =
+  fmap (TokVal . VList) . traverse extractValue . map parseToken . words . unwrap
+  where
+    extractValue (TokVal v) = Just v
+    extractValue _          = Nothing
