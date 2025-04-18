8f765319d78d7709dfc34d89948f8da4a2dd0b80
Johan Fredrik Wilvang <johan@wilvang.com>
2025-04-18 20:25:30 +0200

    feat(parser): Created a parser to tokenize the input string. Each word is parsed trough all the different parsers. If all parsers returns Nothing, the token is set to VSymbol.

--- src/Interpreter/Parser.hs
+++ src/Interpreter/Parser.hs
@@ -1,2 +1,24 @@
--- Tokenize + parse input
 module Interpreter.Parser where
+
+import Text.Read (readMaybe)
+import Data.Maybe (fromMaybe)
+import Control.Applicative ((<|>))
+import qualified Data.Map as M
+import Interpreter.Types
+
+
+-- | Convert an input string into a list of tokens by splitting 
+-- it on whitespace.
+parseTokens :: String -> [Token]
+parseTokens = map parseToken . words
+
+-- | Parse a single string into a 'Token'. This function 
+-- attempts to match the string to different token types
+parseToken :: String -> Token
+parseToken s =
+  fromMaybe (TokVal (VSymbol s)) $
+    maybeParseOp s
+    <|> maybeParseInt s
+    <|> maybeParseFloat s
+    <|> maybeParseBool s
+    <|> maybeParseContext s
