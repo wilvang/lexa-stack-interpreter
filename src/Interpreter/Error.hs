a6fceac6d478af600b4a03c0f593ad9548822f8c
Johan Fredrik Wilvang <johan@wilvang.com>
2025-05-01 14:42:08 +0200

    feat(Error): Created custom ProgramErrors to handle unexpected events in the program execution.

--- src/Interpreter/Error.hs
+++ src/Interpreter/Error.hs
@@ -1,8 +1,26 @@
-module Interpreter.Error (ParserError(..)) where
+module Interpreter.Error (ParserError(..), ProgramError(..)) where
 
+-- | Represents parser errors.
 data ParserError
   = IncompleteQuotation [String]
   | IncompleteList [String]
   | IncompleteString [String]
   | UnexpectedEnd Char
   deriving Show
+
+
+-- | Represents program execution errors.
+data ProgramError =
+     StackEmpty
+   | UnknownSymbol
+   | ExpectedBool
+   | ExpectedBoolOrNumber
+   | ExpectedEnumerable
+   | ExpectedQuotation
+   | ExpectedList
+   | ExpectedVariable
+   | DivisionByZero
+   | ProgramFinishedWithMultipleValues
+   | NumberConversionError
+     deriving (Eq, Show)
+
