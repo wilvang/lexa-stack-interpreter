4a49ff6cf0d5a7329b2d6083d30ae56cd0a86d45
Johan Fredrik Wilvang <johan@wilvang.com>
2025-05-03 18:25:27 +0200

    refactor(Eval): Modify interpret function to handle Either monad during program interpretation.

--- src/Interpreter/Eval.hs
+++ src/Interpreter/Eval.hs
@@ -1,10 +1,9 @@
 -- Stack machine evaluator
-module Interpreter.Eval where
+module Interpreter.Eval (interpret) where
 
-import Text.Read (readMaybe)
-import Interpreter.Types
-import Interpreter.Error (ParserError)
-import Interpreter.Parser 
+import Interpreter.Types (Token(..), Op(..), Value(..))
+import Interpreter.Error (ProgramError(..), BError(..))
+import Interpreter.Parser
 
 type Stack = [Value]
 
@@ -12,17 +11,19 @@ type Stack = [Value]
 -- | Interprets a program given as a string.
 -- Returns either a ParserError if tokenization fails,
 -- or the final Stack after evaluating all tokens.
-interpret :: String -> Either ParserError Stack
+interpret :: String -> Either BError Stack
 interpret input = case parseTokens input of
       Left err     -> Left err
-      Right tokens -> Right (process tokens [])
+      Right tokens -> process tokens []
   where
     -- | Recursively processes a list of tokens, updating the stack at each step.
-    process :: [Token] -> Stack -> Stack
-    process [] stack = stack  -- Base case: no more tokens, return the final stack
-    process (token : tokens) stack = process tokens (step token stack)  -- Apply one token, then continue
+    process :: [Token] -> Stack -> Either BError Stack
+    process [] stack = Right stack  -- Base case: no more tokens, return the final stack
+    process (token : tokens) stack = 
+      case step token stack of
+        Left err       -> Left err  -- If step produces an error, propagate it
+        Right newStack -> process tokens newStack  -- Continue processing with the new stack
 
 
-step :: Token -> Stack -> Stack
+step :: Token -> Stack -> Either BError Stack
 step token stack = -- todo...
-
