ef7342af9091b658301d00c2864ce7313cba9f4f
Johan Fredrik Wilvang <johan@wilvang.com>
2025-05-06 22:52:45 +0200

    refactor(Error): Add new ProgramError 'ExpectedSymbol'.

--- src/Interpreter/Error.hs
+++ src/Interpreter/Error.hs
@@ -19,6 +19,7 @@ data ProgramError =
    | ExpectedQuotation
    | ExpectedList
    | ExpectedVariable
+   | ExpectedSymbol
    | NotComparable
    | DivisionByZero
    | ProgramFinishedWithMultipleValues
