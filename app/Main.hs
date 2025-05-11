8efecab247e2ed9994defe6f6915a57ba42020ab
Johan Fredrik Wilvang <johan@wilvang.com>
2025-05-11 19:01:04 +0200

    feat(Main): Add CLI argument handling for REPL and file execution.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE LambdaCase #-}
 module Main (main) where
 
+import System.Environment (getArgs)
+import Run.REPL (repl, startMessage)
+import Interpreter.State ( State(..), initialStateWithStack)
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = getArgs >>= \case
+  "r" : _   -> putStr startMessage >> repl (initialStateWithStack [] [])
+  (f : _)   -> putStrLn "Running file"
+  []        -> putStrLn "No args"
