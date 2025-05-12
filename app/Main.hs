3cd3214c4c2c3f31f11f5ad7248264321f46a0a1
Johan Fredrik Wilvang <johan@wilvang.com>
2025-05-12 12:06:30 +0200

    refactor(Main): Add documentation the main function.

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,22 @@ import System.Environment (getArgs)
 import Run.REPL (repl, startMessage)
 import Run.File (runFile, importLib)
 
+-- | The main entry point of the program.
+--
+-- This module defines the main function for the application, which handles
+-- command-line arguments and runs the program accordingly. It can either
+-- start the REPL (Read-Eval-Print Loop) or run a script file, depending on
+-- the provided arguments. Additionally, it loads a standard library (`stdlib.lexa`)
+-- to initialize the base state before running any program.
+--
+-- == Behavior:
+--
+-- * If the argument "r" is provided, the program starts the REPL with a predefined
+--   base state (which includes the standard library).
+-- * If a filename is provided as an argument, the program loads and runs the file
+--   with the given base state.
+-- * If no arguments are provided, the program prints "No args was provided."
+--
 main :: IO ()
 main = readFile "app/stdlib.lexa" >>= \libCode ->
     let baseState = importLib libCode in
