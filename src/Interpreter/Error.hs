cd86456cb13f8bd2a6a20c12ed0f365ae4304337
Johan Fredrik Wilvang <johan@wilvang.com>
2025-05-13 13:20:37 +0200

    feat(Error): Add State context to error handling in program execution.

--- src/Interpreter/Error.hs
+++ src/Interpreter/Error.hs
@@ -1,4 +1,32 @@
-module Interpreter.Error (ParserError(..), ProgramError(..), BError(..)) where  
+{-|
+Module      : Interpreter.Error
+Description : Defines the error types for the interpreter.
+
+This module defines the `ParserError`, `ProgramError`, and `BError` types used to represent various errors
+that can occur during the parsing and execution phases of the interpreter. These errors cover issues such as incomplete
+quotations, expected values or symbols, and program execution failures like division by zero or empty list operations.
+
+Data Types:
+-------------
+1. `ParserError`: Represents errors encountered during the parsing phase, such as incomplete quotations or lists.
+2. `ProgramError`: Represents errors that occur during program execution, such as stack errors, unexpected symbols, and type mismatches.
+3. `BError`: A sum type that encapsulates both `ParserError` and `ProgramError`, providing a unified error representation for the interpreter.
+
+Example usage:
+
+* To represent an error during parsing an incomplete string:
+  `ParserError (IncompleteString "some string")`
+
+* To represent a program error due to division by zero:
+  `ProgramError DivisionByZero`
+
+* To wrap a `ProgramError` in a `BError`:
+  `BError (ProgramError DivisionByZero)`
+-}
+
+module Interpreter.Error (ParserError(..), ProgramError(..), BError(..)) where
+  
+import Interpreter.State (State(..))  
 
 -- | Represents parser errors.
 data ParserError
@@ -6,30 +34,29 @@ data ParserError
   | IncompleteList String
   | IncompleteString String
   | UnexpectedEnd Char
-  deriving (Eq, Show)
-
+  deriving (Show)
 
 -- | Represents program execution errors.
 data ProgramError =
-     StackEmpty
+     StackEmpty 
    | UnknownSymbol
    | ExpectedBool
-   | ExpectedBoolOrNumber
-   | ExpectedString
-   | ExpectedEnumerable
-   | ExpectedQuotation
-   | ExpectedList
-   | ExpectedVariable
-   | ExpectedSymbol
-   | NotComparable
-   | DivisionByZero
-   | EmptyList
-   | ProgramFinishedWithMultipleValues
-   | NumberConversionError
-   deriving (Eq, Show)
+   | ExpectedBoolOrNumber 
+   | ExpectedString 
+   | ExpectedEnumerable 
+   | ExpectedList 
+   | ExpectedQuotation State
+   | ExpectedVariable State
+   | ExpectedSymbol State
+   | NotComparable 
+   | DivisionByZero 
+   | EmptyList 
+   | ProgramFinishedWithMultipleValues 
+   | NumberConversionError 
+   deriving (Show)
 
 -- | Represents all program errors
 data BError = 
   ProgramError ProgramError 
   | ParserError ParserError 
-  deriving (Eq, Show)
+  deriving (Show)
